b5ba645802e71ef36680fc2b0aaa974b3138f581
pedro <paarruvito@hotmail.com>
2022-05-31 19:32:24 -0300

    subiendo ejercicio BD

---
+++ BD cac Arruvito.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM alumnos
+LIMIT 0, 1000
+
+-- Date: 2022-05-31 19:17
+*/
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (1,'pedro','arruito','p@arru.com',44,'12/12/1977','Buenos Aires');
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (2,'papa','pepe','p@we.com',33,'01/03/1976','Mendoza');
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (3,'jaja','jeje','j@jeje.com',23,'09/06/2002','La Pampa');
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (4,'Pedro','Arruvito','paarruvito@hotmail.com',77,'30/10/2010','Santa Fe');
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (5,'lele','lala','le@la.com',21,'16/11/2000','CABA');
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (7,'lele','lala','le@la.com',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (10,'lele','lala','le@la.com',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (11,'gg','rr','gg@rr.com',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (16,'hh','rr','hh@gg.com',NULL,NULL,NULL);
+INSERT INTO `` (`id`,`nombre`,`apellido`,`mail`,`edad`,`fecha`,`provincia`) VALUES (17,'hh','rraaa','hh@gg.com',NULL,NULL,NULL);
